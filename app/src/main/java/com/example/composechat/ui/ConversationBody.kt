6e16fe16570cc92a6fc223dfb50c4008d0332f0c
bao do <bao_do@intuit.com>
2022-01-12 22:35:06 -0500

    styling the chat bubbles

--- app/src/main/java/com/example/composechat/ui/ConversationBody.kt
+++ app/src/main/java/com/example/composechat/ui/ConversationBody.kt
@@ -1,28 +1,27 @@
 package com.example.composechat.ui
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.composechat.conversation.ConversationUiState
-import com.example.composechat.conversation.Message
-import com.example.composechat.conversation.initialMessages
+import com.example.composechat.conversation.*
 
 @Composable
 fun ConversationBody(modifier: Modifier = Modifier, ui: ConversationUiState) {
     val scrollState = rememberLazyListState()
     Surface(modifier = modifier) {
-        Messages(ui.messages.reversed(), scrollState = scrollState)
+        Messages(ui.messages, scrollState = scrollState)
     }
 }
 
@@ -34,20 +33,20 @@ fun MessageInput() {
 @Composable
 fun Messages(messages: List<Message>, scrollState: LazyListState = rememberLazyListState()) {
     Box(modifier = Modifier.fillMaxSize()) {
-        LazyColumn(state = scrollState) {
+        LazyColumn(state = scrollState, reverseLayout = true) {
             for (index in messages.indices) {
                 val prevAuthor = messages.getOrNull(index - 1)?.author
                 val nextAuthor = messages.getOrNull(index + 1)?.author
                 val message = messages[index]
-                val isFirstMessageByAuthor = prevAuthor == message.author
-                val isLastMessageByAuthor = nextAuthor == message.author
+                val isFirstMessageByAuthor = prevAuthor != message.author
+                val isLastMessageByAuthor = nextAuthor != message.author
                 val isCurrentAuthor = message.author == "me"
-                val leftOrRightAlign = if (isCurrentAuthor) Modifier.padding(start= 12.dp, end = 48.dp) else Modifier.padding(start = 48.dp, end = 12.dp)
                 item {
-                    Message(modifier = leftOrRightAlign,
+                    Message(
                         message = message,
                         isFirstMessageByAuthor = isFirstMessageByAuthor,
-                        isLastMessageByAuthor = isLastMessageByAuthor
+                        isLastMessageByAuthor = isLastMessageByAuthor,
+                        isAuthorMe = isCurrentAuthor
                     )
                 }
             }
@@ -67,11 +66,67 @@ fun Message(
     modifier: Modifier = Modifier,
     message: Message,
     isFirstMessageByAuthor: Boolean,
-    isLastMessageByAuthor: Boolean
+    isLastMessageByAuthor: Boolean,
+    isAuthorMe: Boolean
 ) {
-    val spaceBetweenAuthors = if (isLastMessageByAuthor) Modifier.padding(top = 8.dp) else Modifier
-    Row(modifier.then(spaceBetweenAuthors)) {
-        Text(text = message.content)
+    val padding = if (isAuthorMe) Modifier.padding(start= 24.dp, end = 8.dp) else Modifier.padding(start = 8.dp, end = 24.dp)
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .then(padding),
+        horizontalAlignment = if (isAuthorMe) Alignment.End else Alignment.Start
+    ) {
+        if (isLastMessageByAuthor) {
+            Spacer(modifier = Modifier.height(24.dp))
+            Text(text = message.author)
+        } else {
+            Spacer(modifier = Modifier.height(12.dp))
+        }
+
+        val backgroundBubbleColor = if (isAuthorMe) {
+            MaterialTheme.colorScheme.primary
+        } else {
+            MaterialTheme.colorScheme.surfaceVariant
+        }
+
+        val chatBubble = if (isAuthorMe) ChatBubbleShapeMe else ChatBubbleShape
+
+        Surface(color = backgroundBubbleColor, shape = chatBubble) {
+            ClickableText(message = message, isAuthorMe = isAuthorMe, {})
+        }
+
     }
 }
+
+@Composable
+fun ClickableText(message: Message, isAuthorMe: Boolean, authorClicked: (String) -> Unit) {
+    val uriHandler = LocalUriHandler.current
+    val styledMessage = messageFormatter(
+        text = message.content,
+        primary = isAuthorMe
+    )
+
+    androidx.compose.foundation.text.ClickableText(
+        text = styledMessage,
+        style = MaterialTheme.typography.bodyLarge.copy(color = LocalContentColor.current),
+        modifier = Modifier.padding(8.dp),
+        onClick = {
+            styledMessage
+                .getStringAnnotations(start = it, end = it)
+                .firstOrNull()
+                ?.let { annotation ->
+                    when (annotation.tag) {
+                        SymbolAnnotationType.LINK.name -> uriHandler.openUri(annotation.item)
+                        SymbolAnnotationType.PERSON.name -> authorClicked(annotation.item)
+                        else -> Unit
+                    }
+                }
+        }
+    )
+}
+
+private val ChatBubbleShape = RoundedCornerShape(4.dp, 20.dp, 20.dp, 20.dp)
+private val ChatBubbleShapeMe = RoundedCornerShape(20.dp, 4.dp, 20.dp, 20.dp)
+
+
 
